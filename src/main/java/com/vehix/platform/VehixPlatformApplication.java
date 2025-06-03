e56428e989cdd1c79250b295ea19cd355a0b9e84
sergioJM05 <sergiogino8@hotmail.com>
2025-06-03 12:37:26 -0500

    chore: initial commit

---
+++ src/main/java/com/vehix/platform/VehixPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.vehix.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehixPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehixPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vehix/platform/VehixPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehix.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehixPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
