cf1063f002e22585c279012e251b64a654a2aabc
AntonioNavarro24 <137426429+AntonioNavarro24@users.noreply.github.com>
2025-06-21 19:22:00 -0500

    feature(monitoring): add entities to monitoring.

---
+++ src/main/java/com/vehix/platform/monitoring/domain/model/entities/BadPractice.java
@@ -0,0 +1,35 @@
+package com.vehix.platform.monitoring.domain.model.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import com.vehix.platform.monitoring.domain.model.commands.CreateBadPracticeCommand;
+
+/**
+ * Represents a bad practice in the system.
+ * This entity is used to track and manage bad practices identified in the system.
+ */
+@Getter
+@Entity
+public class BadPractice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private final String descriptionBadPractice;
+
+    protected BadPractice() {
+        this.descriptionBadPractice = "";
+    }
+
+    public BadPractice(String descriptionBadPractice) {
+        if (descriptionBadPractice == null || descriptionBadPractice.isBlank()) {
+            throw new IllegalArgumentException("Description of Bad Practice cannot be null or empty.");
+        }
+        this.descriptionBadPractice = descriptionBadPractice;
+    }
+
+    public BadPractice(CreateBadPracticeCommand command) {
+        this(command.descriptionBadPractice());
+    }
+}
